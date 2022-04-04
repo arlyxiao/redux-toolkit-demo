949e71f1b7236e00472983f9b3999ddd3aaa21f6
arlyxiao <kingla_pei@163.com>
2022-04-04 12:29:23 +0800

    Remove default code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h5>Welcome to my world</h5>
       </header>
     </div>
   )
